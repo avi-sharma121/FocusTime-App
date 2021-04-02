f61002776bbd115fb4c2edb016f13a020ef3981a
avi-sharma121 <kumaravinashsharma82@protonmail.com>
2021-04-02 06:36:57 +0530

    add time and progressbar function

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import {Text, View, StyleSheet, TextInput} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {Text, StyleSheet} from 'react-native';
 import {colors} from '../utils/colors';
 import {fontSizes, spacing} from '../utils/sizes';
 
 const minutsToMillis = min => min * 60 * 1000;
+const formateTime = time => (time < 10 ? `0${time}` : time);
 
-export const Countdown = ({minutes = 20, isPaused}) => {
-  return <Text style={styles.text}>Count down goes here..</Text>;
+export const Countdown = ({minutes = 0.1, isPaused, onProgress}) => {
+  const [millis, setMillis] = useState(minutsToMillis(minutes));
+  const interval = useRef(null);
+
+  useEffect(() => {
+    if (isPaused) {
+      if (interval.current) clearInterval(interval.current);
+      return;
+    }
+    interval.current = setInterval(countDown, 1000);
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
+
+  const countDown = () => {
+    setMillis(time => {
+      if (time === 0) {
+        return time;
+      }
+      const timeLeft = time - 1000;
+      // onProgress(timeLeft / minutsToMillis(minutes));
+      return timeLeft;
+    });
+  };
+
+  useEffect(() => {
+    onProgress(millis / minutsToMillis(minutes));
+  }, [millis]);
+
+  const minuts = Math.floor(millis / 1000 / 60) % 60;
+  const second = Math.floor(millis / 1000) % 60;
+
+  return (
+    <Text style={styles.text}>
+      {formateTime(minuts)}:{formateTime(second)}
+    </Text>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -14,7 +49,8 @@ const styles = StyleSheet.create({
     fontSize: fontSizes.xxl,
     fontWeight: 'bold',
     color: colors.white,
-    padding: spacing.xl,
+    padding: spacing.md,
     backgroundColor: 'rgba(94,132,226,0.3)',
+    textAlign: 'center',
   },
 });
--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Countdown} from '../../components/Countdown';
 import {colors} from '../../utils/colors';
 import {spacing} from '../../utils/sizes';
+import {RoundedButton} from '../../components/RoundedButton';
+import * as Progress from 'react-native-progress';
+
 export const Timer = ({focusSubject}) => {
+  const [isStarted, setIsStarted] = useState(false);
+  const [progresss, setProgrress] = useState(1);
+
+  const onProgress = progress => {
+    // console.log(progress);
+    setProgrress(progress);
+  };
   return (
     <View style={styles.container}>
-      <Countdown />
+      <View style={styles.countdown}>
+        <Countdown isPaused={!isStarted} onProgress={onProgress} />
+        <View style={{marginTop: 20}}>
+          <Progress.Bar
+            progress={progresss}
+            width={300}
+            height={20}
+            borderRadius={20}
+          />
+        </View>
+      </View>
       <View style={{paddingTop: spacing.xxl}}>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        {isStarted ? (
+          <RoundedButton title="pause" onPress={() => setIsStarted(false)} />
+        ) : (
+          <RoundedButton title="start" onPress={() => setIsStarted(true)} />
+        )}
       </View>
     </View>
   );
@@ -27,5 +54,16 @@ const styles = StyleSheet.create({
     color: colors.white,
     textAlign: 'center',
     fontWeight: 'bold',
+  },
+  countdown: {
+    justifyContent: 'center',
+    flex: 0.5,
+    alignItems: 'center',
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    padding: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
